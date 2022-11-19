ed56d812b135b30d399b6976a2a7474017d63a6d
amiradbrains <amir.adbrains@gmail.com>
2022-11-19 19:17:20 +0530

    professor

--- functions.php
+++ functions.php
@@ -15,6 +15,10 @@ add_action('wp_enqueue_scripts', 'indTech');
 function indTech_features()
 {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('profLandscape', 400, 260, true);
+    add_image_size('profPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
     register_nav_menu('headerMenuLocation', 'Header Menu');
     register_nav_menu('footerLocationOne', 'Footer Location One');
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
